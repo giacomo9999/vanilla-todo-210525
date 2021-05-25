ec37c4740f6e54abaf6c3fcb3fd9b8edc46cc6b4
giacomo9999 <gmarchesi888@gmail.com>
2021-05-25 17:47:18 -0400

    Persist data in localStorage.

--- script.js
+++ script.js
@@ -2,15 +2,31 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const todosUL = document.getElementById("todos");
 
+const todos = JSON.parse(localStorage.getItem("todos"));
+
+const updateLS = () => {
+  todosEl = document.querySelectorAll("li");
+  const todos = [];
+  todosEl.forEach((todoEl) =>
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    })
+  );
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   addTodo();
 });
 
 const addTodo = (todo) => {
+  console.log("addTodo...");
   let todoText = input.value;
   if (todo) todoText = todo.text;
-  
+  console.log("addTodo...", todo);
+
   if (todoText) {
     const todoEl = document.createElement("li");
     if (todo && todo.completed) {
@@ -18,16 +34,25 @@ const addTodo = (todo) => {
     }
     todoEl.innerText = todoText;
 
-    todoEl.addEventListener("click", () =>
-      todoEl.classList.toggle("completed")
-    );
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      updateLS();
+    });
 
     todoEl.addEventListener("contextmenu", (e) => {
       e.preventDefault();
       todoEl.remove();
+      updateLS();
     });
 
     todosUL.appendChild(todoEl);
     input.value = "";
+
+    updateLS();
   }
 };
+
+if (todos) {
+  console.log("getting data from localStorage...", todos);
+  todos.forEach((todo) => addTodo(todo));
+}
